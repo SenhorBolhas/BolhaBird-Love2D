74928b3e89774fad425da29b083d6bcdccc6f2f7
Giovanni Pelloso Gasparetto <32843196+SenhorBolhas@users.noreply.github.com>
2020-11-16 18:38:38 -0300

    Add files via upload

---
+++ Bolha Bird/PipePair.lua
@@ -0,0 +1,61 @@
+--[[
+    PipePair Class
+
+    Author: Giovanni Pelloso Gasparetto
+    giovanni_pelloso@hotmail.com
+
+    Used to represent a pair of pipes that stick together as they scroll, providing an opening
+    for the player to jump through in order to score a point.
+]]
+
+PipePair = Class{}
+
+-- size of the gap between pipes
+local MIN_GAP_HEIGHT = 90
+local MAX_GAP_HEIGHT = 120
+
+function PipePair:init(y)
+    -- flag to hold whether this pair has been scored (jumped through)
+    self.scored = false
+
+    -- initialize pipes past the end of the screen
+    self.x = VIRTUAL_WIDTH + 32
+
+    -- y value is for the topmost pipe; gap is a vertical shift of the second lower pipe
+    self.y = y
+
+    local teste = math.random(MIN_GAP_HEIGHT, MAX_GAP_HEIGHT)
+    -- instantiate two pipes that belong to this pair
+    self.pipes = {
+        ['upper'] = Pipe('top', self.y),
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + math.random(MIN_GAP_HEIGHT, MAX_GAP_HEIGHT))
+    }
+
+    -- whether this pipe pair is ready to be removed from the scene
+    self.remove = false
+end
+
+function PipePair:update(dt)
+    -- remove the pipe from the scene if it's beyond the left edge of the screen,
+    -- else move it from right to left
+    if self.x > -PIPE_WIDTH then
+        self.x = self.x - PIPE_SPEED * dt
+        self.pipes['lower'].x = self.x
+        self.pipes['upper'].x = self.x
+    else
+        self.remove = true
+    -- increasing difficult over time reducing the gap between the pipes
+        if (MAX_GAP_HEIGHT >=91) then
+            MAX_GAP_HEIGHT = MAX_GAP_HEIGHT - 1
+        end
+        if (MAX_GAP_HEIGHT <= 100 and MIN_GAP_HEIGHT >= 80) then
+            MIN_GAP_HEIGHT = MIN_GAP_HEIGHT - 1
+        end
+   end
+end
+
+function PipePair:render()
+    for l, pipe in pairs(self.pipes) do
+        pipe:render()
+    end
+end
